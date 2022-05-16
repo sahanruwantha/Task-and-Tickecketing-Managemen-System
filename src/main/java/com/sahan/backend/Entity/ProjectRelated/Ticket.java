9302f9e3cd59e9715dccb8541fc644e94251e618
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-05-16 16:00:42 +0530

    updated jpa Entities

--- src/main/java/com/sahan/backend/Entity/ProjectRelated/Ticket.java
+++ src/main/java/com/sahan/backend/Entity/ProjectRelated/Ticket.java
@@ -1,11 +1,15 @@
 package com.sahan.backend.Entity.ProjectRelated;
 
+import com.sahan.backend.Entity.User;
+
 import java.util.Date;
+import java.util.List;
 
 public class Ticket
 {
     private long ID;
     private String Name;
     private Date Deadline;
+    private List<User> assignedEngineers;
 
 }
