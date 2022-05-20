3930aaf691cc1f3f7223de7ce91aa2fa8d6a93f2
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-05-20 09:04:41 +0530

    updated services

--- src/main/java/com/sahan/backend/Service/TaskService.java
+++ src/main/java/com/sahan/backend/Service/TaskService.java
@@ -1,7 +1,10 @@
 package com.sahan.backend.Service;
 
+import com.sahan.backend.Entity.ProjectRelated.Project;
 import com.sahan.backend.Entity.ProjectRelated.Task;
+import com.sahan.backend.Repository.ProjectRepository;
 import com.sahan.backend.Repository.TaskRepository;
+import com.sahan.backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,15 @@ public class TaskService
     @Autowired
     private TaskRepository taskRepository;
 
-    public Task createNewTask(String name, Long taskID, Set<Long> userIDs)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Task createNewTask(String name, Long projectID, Set<Long> userIDs)
     {
+        Project project = projectRepository.findById(projectID).orElse(null);
         return taskRepository.save(task);
     }
 }
